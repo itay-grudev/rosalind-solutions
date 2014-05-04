9ef53b04b2957599c2d48fe4d2dc7de1ac5f86e9
Itay Grudev <itay.grudev@gmail.com>
2014-05-04 18:26:57 +0300

    PROT

---
+++ solutions/prot.rb
@@ -0,0 +1,17 @@
+class Task_PROT < Task
+	def task(input)
+		index = 0
+		mRNA = ''
+		(input.length / 3).times do |i|
+			codone = input[index...index+3]
+			acid = Constants::RNA2mRNA[codone]
+			if acid == :Stop
+				break
+			end
+			mRNA += acid.to_s
+			index += 3
+		end
+
+		puts mRNA
+	end
+end
