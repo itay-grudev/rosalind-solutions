19d3e88dc061125e6f822cfc7e65779d2f381b45
Itay Grudev <itay.grudev@gmail.com>
2014-05-04 19:07:56 +0300

    FIB

---
+++ solutions/fib.rb
@@ -0,0 +1,18 @@
+class Task_FIB < Task
+	def task(input)
+		n, k = input.split(' ').map { |v| v.to_i }
+
+		an = [1, 1]
+		if n < 3
+			return 1
+		else
+			for	i in 3 .. n
+				t = an[0]
+				an[0] = an[0] + an[1] * k
+				an[1] = t
+			end
+		end
+
+		puts an[0]
+	end
+end
