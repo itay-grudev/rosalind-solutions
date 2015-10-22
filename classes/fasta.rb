f3d70155b0fb81a94bd8a9eede8fac6b097f2e3d
Itay Grudev <itay@grudev.com>
2015-10-22 13:42:45 +0100

    Improved Fasta class
    It now extend from Forwardable and includes Enumerable, thus you can
    use it directly like an array of Fasta::Sequence. Which also now
    inherits from String so you can access the DNA String directly

--- classes/fasta.rb
+++ classes/fasta.rb
@@ -1,11 +1,26 @@
+require 'forwardable'
+
 class Fasta
-	class Sequence
-		attr_accessor :description, :dna_string
+  extend Forwardable
+  include Enumerable
+  def_delegators :@sequences, :size, :length, :each, :<<, :[], :first, :last
+
+  class InvalidSequenceAssignment < Exception; end
+
+	class Sequence < String
+		attr_accessor :description
 
 		def initialize
 			@description = ''
-			@dna_string = ''
 		end
+
+    def dna_string
+      self
+    end
+
+    def dna_string=(value)
+      replace value
+    end
 
 		def to_s(line_length = 60)
 			'>' + @description + "\n" + @dna_string.scan(/.{#{line_length}}|.+/).join("\n")
@@ -14,8 +29,8 @@ class Fasta
 	end
 
 	def self.from_string(input)
-		sequence = nil		
-		fasta = []
+		sequence = nil
+		fasta = Fasta.new
 
 		lines = input.split("\n")
 
@@ -25,12 +40,28 @@ class Fasta
 			end
 
 			if lines[i][0] == '>' or i + 1 == lines.length  # description line or last line
-				fasta << sequence if i > 0
+        fasta << sequence if i > 0
 				sequence = Sequence.new
 				sequence.description = lines[i][1..-1]
 			end
 		end
 
-		fasta # An array of Fasta::Sequence :D
+    fasta
 	end
+
+  def []=(index, value)
+    if value.is_a? Fasta::Sequence
+      return @sequences[index] = value
+    else
+      raise Fasta::InvalidSequenceAssignment
+    end
+  end
+
+  def initialize
+    @sequences = []
+  end
+
+  def to_s(line_length = 60)
+    @sequences.map { |s| s.to_s(line_length) }.join("\n")
+  end
 end
